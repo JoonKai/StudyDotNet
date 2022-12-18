8dd9525be6dd5638772cae946571b38619c0d8b9
thstj <joonseok831@gmail.com>
2022-12-18 23:44:41 +0900

    221218

---
+++ Chapter02/Chart2DSystem.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Chapter02
+{
+    /// <summary>
+    /// Chart2DSystem.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Chart2DSystem : Window
+    {
+        private double xMin = 0.0;
+        private double xMax = 10.0;
+        private double yMin = 0.0;
+        private double yMax = 10.0;
+        private Line line1;
+        private Polyline polyline1;
+
+        public Chart2DSystem()
+        {
+            InitializeComponent();
+            AddGraphics();
+        }
+
+        private void AddGraphics()
+        {
+            line1= new Line();
+            line1.X1 = XNomalize(2.0);
+            line1.Y1 = YNomalize(4.0);
+            line1.X2 = XNomalize(8.0);
+            line1.Y2 = YNomalize(10.0);
+            line1.Stroke = Brushes.Blue;
+            line1.StrokeThickness = 2;
+            plotCanvas.Children.Add(line1);
+
+            polyline1 = new Polyline();
+            polyline1.Points.Add(new Point(XNomalize(8), YNomalize(8)));
+            polyline1.Points.Add(new Point(XNomalize(6), YNomalize(6)));
+            polyline1.Points.Add(new Point(XNomalize(6), YNomalize(4)));
+            polyline1.Points.Add(new Point(XNomalize(4), YNomalize(4)));
+            polyline1.Points.Add(new Point(XNomalize(4), YNomalize(6)));
+            polyline1.Points.Add(new Point(XNomalize(6), YNomalize(6)));
+            polyline1.Stroke = Brushes.Red;
+            polyline1.StrokeThickness = 5;
+            plotCanvas.Children.Add(polyline1);
+        }
+        private double XNomalize(double x)
+        {
+            double result = (x - xMin) * plotCanvas.Width / (xMax - xMin);
+            return result;
+        }
+
+        private double YNomalize(double y)
+        {
+            double result = plotCanvas.Height - (y - yMin) * plotCanvas.Height / (yMax - yMin);
+            return result;
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            xMin = Convert.ToDouble(tbXMin.Text);
+            xMax = Convert.ToDouble(tbXMax.Text);
+            yMin = Convert.ToDouble(tbYMin.Text);
+            yMax = Convert.ToDouble(tbYMax.Text);
+            plotCanvas.Children.Remove(line1);
+            plotCanvas.Children.Remove(polyline1);
+            AddGraphics();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Chapter03/ObjectMatrixTransforms.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Chapter03
+{
+    /// <summary>
+    /// ObjectMatrixTransforms.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class ObjectMatrixTransforms : Window
+    {
+        public ObjectMatrixTransforms()
+        {
+            InitializeComponent();
+        }
+
+        private void btnApply_Click(object sender, RoutedEventArgs e)
+        {
+            Matrix m = new Matrix();
+            m.M11 = Double.Parse(tbM11.Text);
+            m.M12= Double.Parse(tbM12.Text);
+            m.M21= Double.Parse(tbM21.Text);
+            m.M22= Double.Parse(tbM22.Text);
+            m.OffsetX = double.Parse(tbOffsetX.Text);
+            m.OffsetY= double.Parse(tbOffsetY.Text);
+            matrixTransform.Matrix= m;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
