08f1c952a57d8e43549a227665bd6f2652599d50
thstj <thstj@SEOK>
2023-05-17 07:24:04 +0900

    3333

--- WPFDatagrid2/MainWindow.xaml.cs
+++ WPFDatagrid2/MainWindow.xaml.cs
@@ -1,17 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
-namespace WPFDatagrid
+namespace WPFDatagrid2
 {
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
-            //DG.ItemsSource = Songs.GetSongs();
         }
     }
     public class Songs
@@ -20,9 +32,9 @@ namespace WPFDatagrid
         public static List<Song> GetSongs()
         {
             var file = @"C:\Users\thstj\OneDrive\문서\songs.csv";
-            var lines=File.ReadAllLines(file);
-            var list= new List<Song>();
-            for (int i = 0; i<lines.Length; i++)
+            var lines = File.ReadAllLines(file);
+            var list = new List<Song>();
+            for (int i = 0; i < lines.Length; i++)
             {
                 var line = lines[i].Split(',');
                 var g = line[2].Split(' ', '&', '-');
@@ -32,11 +44,11 @@ namespace WPFDatagrid
                     Id = int.Parse(line[0]),
                     Title = line[1],
                     Artist = line[3],
-                    IsSoundtrack = line[4]=="Unknown"?false:true,
+                    IsSoundtrack = line[4] == "Unknown" ? false : true,
                     MovieTitle = line[4],
-                    Genre=(Genre)Enum.Parse(typeof(Genre), gr),
-                    ResealeYear= DateTime.Parse(line[5]+",1,1"),
-                    URL=new Uri($"WWW.{line[3]}.com",UriKind.Relative),
+                    Genre = (Genre)Enum.Parse(typeof(Genre), gr),
+                    ResealeYear = DateTime.Parse(line[5] + ",1,1"),
+                    URL = new Uri($"WWW.{line[3]}.com", UriKind.Relative),
                 };
                 list.Add(song);
             }
@@ -48,7 +60,7 @@ namespace WPFDatagrid
         public int Id { get; set; }
         public string Title { get; set; }
         public string Artist { get; set; }
-        public bool IsSoundtrack { get;set; }
+        public bool IsSoundtrack { get; set; }
         public string MovieTitle { get; set; }
         public Genre Genre { get; set; }
         public DateTime ResealeYear { get; set; }
