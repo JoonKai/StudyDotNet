92f27ed9e54e91c55436eeb4358f56bfa66738de
준석 손 <thstjr0487@naver.com>
2023-08-19 17:49:34 +0900

    1111

---
+++ DependencyInjectionProject2/DateTimeService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionProject2
+{
+    public class DateTimeService : IDateTimeService
+    {
+        public string GetDateTimeString()
+        {
+            return DateTime.Now.ToLongTimeString();
+        }
+    }
+}
